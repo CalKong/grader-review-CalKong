0d62f9cf69cd6078f0a3cc86bb7054c4e97f56e2
CalKong <calvin.j.kong@gmail.com>
2023-11-28 10:38:11 -0800

    updated grading script

--- TestListExamples.java
+++ TestListExamples.java
@@ -8,7 +8,6 @@ class IsMoon implements StringChecker {
     return s.equalsIgnoreCase("moon");
   }
 }
-
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
@@ -18,4 +17,41 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testFilter(){
+    List<String> argList = Arrays.asList("moon", "moon1", "MOON", "moOn");
+    List<String> expected = Arrays.asList("moon", "MOON", "moOn");
+    StringChecker sc = new IsMoon();
+    assertEquals(expected, ListExamples.filter(argList,sc));
+
+  }
+  @Test(timeout = 500)
+  public void testMergeLeftEnd(){
+    List<String> left = Arrays.asList("a", "d");
+    List<String> right = Arrays.asList("a", "b", "c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testAlternate(){
+    List<String> left = Arrays.asList("a", "c", "e");
+    List<String> right = Arrays.asList("b", "d", "f");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout= 500)
+  public void diffLength(){
+    List<String> left = Arrays.asList("a", "b", "c", "d", "e", "z");
+    List<String> right = Arrays.asList("d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "d", "e", "z");
+    assertEquals(expected, merged);
+  }
+
+
 }
--- grade.sh
+++ grade.sh
@@ -1,12 +1,26 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission > /dev/null 2>&1
 echo 'Finished cloning'
+
+# Check if the correct file is submitted
+if [ -e "student-submission/ListExamples.java" ]; then
+    echo "Student submitted the correct file."
+else
+    echo "Incorrect file submitted. Please submit a repository with a file named ListExamples.java."
+    exit 1
+fi
+
+# Copy the student's code to the grading-area directory
+cp "student-submission/ListExamples.java" "grading-area/ListExamples.java"
+
+# Copy the grading tests to the grading-area directory
+cp "TestListExamples.java" "grading-area/TestListExamples.java"
 
 
 # Draw a picture/take notes on the directory structure that's set up after
@@ -14,3 +28,43 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+# Compile the files
+
+cd grading-area
+
+javac -cp $CPATH *.java 2> error.txt
+
+# Check the exit status of the compilation
+
+if [ $? -eq 0 ]; then
+    # Run the JUnit tests
+    java -cp "$CPATH" org.junit.runner.JUnitCore TestListExamples > JUNIT_OUTPUT.txt
+    #cat JUNIT_OUTPUT.txt
+
+    # Calculate grade 
+    
+    # Search for the line containing information about passed tests
+    #passed_line= `grep "OK" JUNIT_OUTPUT.txt`
+    passed_line=$(grep "OK" JUNIT_OUTPUT.txt)
+
+    #Extract the number of passed tests from the line
+    if [ -n "$passed_line" ]; then
+        grade=$(echo "$passed_line" | grep -oE '[0-9]+')
+        echo "Grade : $grade/$grade"
+    else 
+        num_tests_line=$(grep -oE "Tests run: [0-9]+" JUNIT_OUTPUT.txt)
+        numTests=$(echo "$num_tests_line" | grep -oE '[0-9]+')
+        num_failed_line=$(grep -oE "Failures: [0-9]+" JUNIT_OUTPUT.txt)
+        numFailed=$(echo "$num_tests_line" | grep -oE '[0-9]+')
+        numCorrect=$((numTests - numFailed))
+        echo "Grade : $numCorrect/$numTests"
+
+    fi
+else
+    echo "Compilation failed. Please check your code."
+    exit 1
+fi
+
+
